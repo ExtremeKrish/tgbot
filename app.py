5052d40bbea10165885fa1fc26ef196a23c07067
Krish Vishwakarma <79414142+ExtremeKrish@users.noreply.github.com>
2023-11-19 19:40:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
-import requests
+from telegram.ext import Updater, MessageHandler, Filters
 
-def send_telegram_message(bot_token, chat_id, message):
-    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
-    params = {'chat_id': chat_id, 'text': message}
+# Replace 'YOUR_BOT_TOKEN' with your actual bot token
+TOKEN = '6629025964:AAEx3-t_AmAy-vGK3Gk0JH2f7a61EisDxsk'
 
-    response = requests.post(url, params=params)
+def repeat_message(update, context):
+    user_input = update.message.text
+    update.message.reply_text(f"You said: {user_input}")
 
-    if response.status_code == 200:
-        print("Message sent successfully")
-    else:
-        print(f"Failed to send message. Status code: {response.status_code}")
+def main():
+    updater = Updater(TOKEN)
+    dp = updater.dispatcher
 
-# Replace 'YOUR_BOT_TOKEN' and 'YOUR_CHAT_ID' with your actual bot token and user chat ID
-bot_token = '6171891363:AAEgSTM5I5w2ljZEl4uJ6cdgFmHJWqnHOmQ'
-chat_id = '6162808595'
-message_text = 'Hello, this is a test message from your Telegram bot!'
+    # Register a message handler to respond to text messages
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, repeat_message))
 
-send_telegram_message(bot_token, chat_id, message_text)
-exit()
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
